c22f0d20f67c420070593385f12df9138c9d8410
erallesko <eraldolesko@gmail.com>
2025-05-09 06:50:24 +0100

    adding get Users function

---
+++ utils/getUsers.js
@@ -0,0 +1,30 @@
+function getUsers(objects){
+
+    const arrays = objects.map((object) => {
+
+        const element = [];
+        let host = false;
+
+        for (let key in object){
+
+            if (key === "is_host"){
+                if (object[key] === "host"){
+                    host = true
+                    element.push(host)
+                }else {
+                    element.push(host)
+                 };
+            }else {
+                element.push(object[key])
+            }
+        };
+
+        return element;
+    })
+    
+
+
+    return arrays;
+}
+
+module.exports = getUsers;
